36576c38547a0542967cc36f2ddfcda7a81aa779
Josh <59369632+bushkabob@users.noreply.github.com>
2023-11-20 12:00:39 -0500

    add eeg blink detection

--- acc_plot.py
+++ acc_plot.py
@@ -3,51 +3,112 @@
 # the data is in the form of a csv file
 # plot it after reading it
 import csv
+import math
+import time
 import matplotlib.pyplot as plt
+from pylsl import StreamInlet, resolve_stream
+import msvcrt
 
-def plot_and_read():
-    # Specify the path to your CSV file
-    csv_file_path = 'ACC_recording_2023-11-14-18.25.54.csv'
-    datax = []
-    datay = []
-    dataz = []
-    timestamps = []
-    filter = 0
-    # Open the CSV file in read mode
-    with open(csv_file_path, 'r') as file:
-        # Create a CSV reader object
-        csv_reader = csv.reader(file)
-        #skip the first row
-        next(csv_reader)
-        # Iterate over each row in the CSV file
-        for row in csv_reader:
-            #print(row)
-            timestamps.append(float(row[0]))
-            datax.append(float(row[1]))
-            if float(row[2]) > filter or float(row[2]) < (-1 * filter):
-                datay.append(float(row[2]))
-            else:
-                datay.append(0)
-            dataz.append(float(row[3]))
+# def plot_and_read():
+#     # Specify the path to your CSV file
+#     csv_file_path = 'ACC_recording_2023-11-14-18.25.54.csv'
+#     datax = []
+#     datay = []
+#     dataz = []
+#     timestamps = []
+#     filter = 0
+#     # Open the CSV file in read mode
+#     with open(csv_file_path, 'r') as file:
+#         # Create a CSV reader object
+#         csv_reader = csv.reader(file)
+#         #skip the first row
+#         next(csv_reader)
+#         # Iterate over each row in the CSV file
+#         for row in csv_reader:
+#             #print(row)
+#             timestamps.append(float(row[0]))
+#             datax.append(float(row[1]))
+#             if float(row[2]) > filter or float(row[2]) < (-1 * filter):
+#                 datay.append(float(row[2]))
+#             else:
+#                 datay.append(0)
+#             dataz.append(float(row[3]))
 
-    #determine line of best fit for data y
-    #y = mx + b
-    m = (datay[699] - datay[0]) / (timestamps[699] - timestamps[0])
-    b = datay[0] - m * timestamps[0]
-    # for i in range(len(datay)):
-    #     datay[i] = datay[i] - (m * timestamps[i] + b)
+#     #determine line of best fit for data y
+#     #y = mx + b
+#     m = (datay[699] - datay[0]) / (timestamps[699] - timestamps[0])
+#     b = datay[0] - m * timestamps[0]
+#     # for i in range(len(datay)):
+#     #     datay[i] = datay[i] - (m * timestamps[i] + b)
 
-    # sums = [0]
-    # old_time = timestamps[0]
-    # for i in range(1,len(timestamps)):
-    #     val = 1/2 * (datay[i] + datay[i-1]) * (timestamps[i] - old_time)
-    #     sums.append(sums[i-1] + val)
-    #     old_time = timestamps[i]
+#     # sums = [0]
+#     # old_time = timestamps[0]
+#     # for i in range(1,len(timestamps)):
+#     #     val = 1/2 * (datay[i] + datay[i-1]) * (timestamps[i] - old_time)
+#     #     sums.append(sums[i-1] + val)
+#     #     old_time = timestamps[i]
 
-    #plt.plot(timestamps, datax)
-    plt.plot(datay)
-    #plt.plot(timestamps, dataz)
-    #plt.plot(timestamps, data)
+#     #plt.plot(timestamps, datax)
+#     plt.plot(datay)
+#     #plt.plot(timestamps, dataz)
+#     #plt.plot(timestamps, data)
+#     plt.show()
+
+channel1 = []
+channel2 = []
+channel3 = []
+channel4 = []
+channel5 = []
+timestamps = []
+
+def read():
+    streams = resolve_stream('type', 'EEG')
+    inlet = StreamInlet(streams[0])
+    init_sample, init_timestamp = inlet.pull_sample()
+    timestamp = init_timestamp
+    while(timestamp - init_timestamp < 5):
+        sample, timestamp = inlet.pull_sample()
+        channel3.append(sample[2])
+        
+
+    plt.plot(channel3)
     plt.show()
+    mx = max(channel3)
+    mn = min(channel3)
+    range = (mx - mn)
+    avg = sum(channel3)/len(channel3)
 
-plot_and_read()
+    run = True
+    print(avg)
+
+    blink_time = init_timestamp
+
+    while run:
+        sample, timestamp = inlet.pull_sample()
+        #print(timestamp, sample)
+        timestamps.append(timestamp)
+        channel1.append(sample[0])
+        channel2.append(sample[1])
+        channel3.append(sample[2])
+        channel4.append(sample[3])
+        channel5.append(sample[4])
+        if((sample[2] > avg + range or sample[2] < avg - range) and timestamp - blink_time > 1):
+            blink_time = timestamp
+            print("blink")
+
+        if msvcrt.kbhit():
+            key = msvcrt.getch()
+            print("key pressed")
+            run = False
+
+read()
+plt.plot(channel1)
+plt.show()
+plt.plot(channel2)
+plt.show()
+plt.plot(channel3)
+plt.show()
+plt.plot(channel4)
+plt.show()
+plt.plot(channel5)
+plt.show()
---
+++ main_improved.py
@@ -0,0 +1,212 @@
+import msvcrt
+from pylsl import StreamInlet, resolve_stream
+from scipy.integrate import quad
+from translator import send_byte, thing, init_arm, close
+import threading
+import time
+import signal
+import sys
+import matplotlib.pyplot as plt
+
+def integrand(x, a, b):
+    return a*x**2 + b
+
+def start_arm():
+    init_arm()
+    thing()
+
+def send_to_arm(max, min, val, letter, inverse):
+    # convert val to a value between 0 and 180 using the range [min, max]
+    # send the value to the arm
+    if(max == min):
+        return
+    range = abs(max - min)
+    new_val = abs(val - min) / range * 180
+    if inverse:
+        new_val = 180 - new_val
+    print(max, min, val, int(new_val))
+    ascii_num = chr(int(new_val))
+    send_byte(letter, ascii_num)
+
+def determine_drift(timestamps, data):
+    # determine the drift of the data
+    # return the drift in the form (m, b)
+    # where y = mx + b
+    # total_slope_sum = 0
+    # total_slope_count = 0
+    # for i in range(1, len(data)):
+    #     if timestamps[i] - timestamps[i-1] > 0:
+    #         total_slope_count += 1
+    #         total_slope_sum += (data[i] - data[i-1]) / (timestamps[i] - timestamps[i-1])
+    # m = total_slope_sum / (total_slope_count - 1)
+    # b = data[0] - m * timestamps[0]
+    # print("approx: " + str((data[len(data) - 1] - data[0]) / (timestamps[len(timestamps) - 1] - timestamps[0])))
+    sum_data = sum(data)
+    m = sum_data / len(data)
+    # plt.plot(timestamps, data)
+    # plt.show()
+    return (m, 0)
+
+def calibrate(intlet):
+    print("Remain still for 5 seconds")
+    init_sample, init_timestamp = intlet.pull_sample()
+    timestamps = [init_timestamp]
+    datax = [init_sample[0]]
+    datay = [init_sample[1]]
+    dataz = [init_sample[2]]
+    sumx = [0]
+    sumy = [0]
+    sumz = [0]
+    #store data to arrays
+    while(timestamps[len(timestamps) - 1] - init_timestamp < 5):
+        sample, new_timestamp = intlet.pull_sample()
+        datax.append(sample[0])
+        datay.append(sample[1])
+        dataz.append(sample[2])
+        timestamps.append(new_timestamp)
+    #retroactively integrate
+    for i in range(1, len(datax)):
+        valx = 1/2 * (datax[i] + datax[i-1]) * (timestamps[i] - timestamps[i-1])
+        sumx.append(sumx[len(sumx)-1] + valx)
+        valy = 1/2 * (datay[i] + datay[i-1]) * (timestamps[i] - timestamps[i-1])
+        sumy.append(sumy[len(sumy)-1] + valy)
+        valz = 1/2 * (dataz[i] + dataz[i-1]) * (timestamps[i] - timestamps[i-1])
+        sumz.append(sumz[len(sumz)-1] + valz)
+    #timestamps.pop(0)
+    #print(timestamps)
+    sumx.pop(0)
+    sumy.pop(0)
+    sumz.pop(0)
+    #determine the respective drifts
+    x_drift = determine_drift(timestamps, datax)
+    y_drift = determine_drift(timestamps, datay)
+    z_drift = determine_drift(timestamps, dataz)
+
+    print(timestamps[len(timestamps) - 1] - timestamps[1])
+    print(sumy[1], sumy[len(sumy) - 1])
+    print(x_drift, y_drift, z_drift)
+    # plt.plot(timestamps, sumy)
+    # plt.show()
+    return (x_drift, y_drift, z_drift)
+
+def main():
+    print("starting arm...")
+    threading.Thread(target=start_arm).start()
+    print("waiting for arm to boot...")
+    #time.sleep(30)
+    print("arm booted")
+    # first resolve an GYRO stream on the lab network
+    print("looking for an GYRO stream...")
+    streams = resolve_stream('type', 'GYRO')
+    print("resolving stream...")
+    # create a new inlet to read from the stream
+    inlet = StreamInlet(streams[0])
+    #calibrate
+    print("calibrating...")
+    calibration = calibrate(inlet)
+    print("calibrated")
+    #wait for arm to boot
+    
+    run = True
+    init_sample, init_timestamp = inlet.pull_sample()
+    datax = [init_sample[0]]
+    datay = [init_sample[1]]
+    dataz = [init_sample[2]]
+    timestamps = [init_timestamp]
+    sumx = [0]
+    sumy = [0]
+    sumz = [0]
+    max_and_minx = (0,0)
+    max_and_miny = (0,0)
+    max_and_minz = (0,0)
+    old_time = init_timestamp
+    old_time_write = 0
+
+    while run:
+        # get a new sample (you can also omit the timestamp part if you're not
+        # interested in it)
+        sample, timestamp = inlet.pull_sample()
+        #print(timestamp, sample)
+        override_val = 0
+        if sample[0] < override_val and sample[0] > -override_val:
+            sample[0] = 0
+        if sample[1] < override_val and sample[1] > -override_val:
+            sample[1] = 0
+        if sample[2] < override_val and sample[2] > -override_val:
+            sample[2] = 0
+        
+        sample[1] = sample[1] - calibration[1][0]
+        sample[2] = sample[2] - calibration[2][0]
+
+        valx = quad(integrand, old_time, timestamp, args=(datax[len(datax)-1], sample[0]))
+        #valx = valx[0] - calibration[0][0] * (timestamp - old_time)
+        valx = valx[0]
+        sumx.append(sumx[len(sumx)-1] + valx)
+        valy = 1/2 * (datay[len(datay) - 1] + sample[1]) * (timestamp - old_time) 
+        valy = valy #- calibration[1][0] * (timestamp - old_time)
+        sumy.append(sumy[len(sumy)-1] + valy)
+
+        valz = quad(integrand, old_time, timestamp, args=(dataz[len(dataz)-1], sample[2]))
+        valz = valz[0] #- calibration[2][0] * (timestamp - old_time)
+        sumz.append(sumz[len(sumz)-1] + valz)
+
+        datax.append(sample[0])
+        datay.append(sample[1])
+        dataz.append(sample[2])
+
+        timestamps.append(timestamp)
+
+        if(sumx[len(sumx) - 1] > max_and_minx[0]):
+            max_and_minx = (sumx[len(sumx) - 1], max_and_minx[1])
+        if(sumx[len(sumx) - 1] < max_and_minx[1]):
+            max_and_minx = (max_and_minx[0], sumx[len(sumx) - 1])
+        
+        if(sumy[len(sumy) - 1] > max_and_miny[0]):
+            max_and_miny = (sumy[len(sumy) - 1], max_and_miny[1])
+        if(sumy[len(sumy) - 1] < max_and_miny[1]):
+            max_and_miny = (max_and_miny[0], sumy[len(sumy) - 1])
+        
+        if(sumz[len(sumz) - 1] > max_and_minz[0]):
+            max_and_minz = (sumz[len(sumz) - 1], max_and_minz[1])
+        if(sumz[len(sumz) - 1] < max_and_minz[1]):
+            max_and_minz = (max_and_minz[0], sumz[len(sumz) - 1])
+
+        #if(len(datax) > 1):
+            #sumx.append(1/2 * (datax[len(datax) - 2] + datax[len(datax) - 1]) * (timestamp - old_time))
+            #sumy.append(1/2 * (datay[len(datay) - 2] + datay[len(datay) - 1]) * (timestamp - old_time))
+            #sumz.append(1/2 * (dataz[len(dataz) - 2] + dataz[len(dataz) - 1]) * (timestamp - old_time))
+        old_time = timestamp
+        #detect a keypress and set run to false
+        if msvcrt.kbhit():
+            key = msvcrt.getch()
+            print("key pressed")
+            run = False
+        
+        if(timestamp - old_time_write > 0.2):
+            send_to_arm(max_and_minz[0], max_and_minz[1], sumz[len(sumz) - 1], 'D', True)
+            send_to_arm(max_and_miny[0], max_and_miny[1], sumy[len(sumy) - 1], 'E', False)
+            old_time_write = timestamp
+
+    #plot
+    # plt.plot(datax)
+    # plt.plot(datay)
+    # plt.plot(dataz)
+    #plt.show()
+    plt.plot(timestamps, datay)
+    plt.show()
+    plt.plot(timestamps, dataz)
+    plt.show()
+    plt.plot(timestamps, sumy)
+    plt.show()
+    plt.plot(timestamps, sumz)
+    plt.show()
+
+def handle_signal(signum, frame):
+    print(f'handling signal {signum}')
+    # do cleanup
+    close()
+    sys.exit(0)
+
+if __name__ == '__main__':
+    signal.signal(signal.SIGINT, handle_signal)
+    main()
